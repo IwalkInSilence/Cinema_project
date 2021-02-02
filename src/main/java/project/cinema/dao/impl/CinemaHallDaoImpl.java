ff046f905c425a4c3938033472bfe300fc628073
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2021-02-02 13:38:14 +0200

    fix empty line

--- src/main/java/project/cinema/dao/impl/CinemaHallDaoImpl.java
+++ src/main/java/project/cinema/dao/impl/CinemaHallDaoImpl.java
@@ -45,4 +45,3 @@ public class CinemaHallDaoImpl implements CinemaHallDao {
         }
     }
 }
-
--- src/main/java/project/cinema/model/MovieSession.java
+++ src/main/java/project/cinema/model/MovieSession.java
@@ -2,13 +2,7 @@ package project.cinema.model;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "movie_session")
